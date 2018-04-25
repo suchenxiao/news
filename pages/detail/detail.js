c5f1ec03cc909f8c6f31561907d176d900ed5798
suchenxiao <suchenxiao@foxmail.com>
2018-04-25 21:54:24 +0800

    Feature: set api data in deatil-page

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -20,5 +20,43 @@ Page({
           }
         ]
       }
+  },
+
+  onLoad: function () {
+    this.getNews();
+  },
+
+  getNews: function (callback) {
+    wx.request({
+      url: 'https://test-miniprogram.com/api/news/detail',
+      data: {
+        id: 1523074607667
+      },
+      success: res => {
+        let result = res.data.result;
+        if (res.data.code === 200) {
+          console.log(res);  
+          this.setNews(result);
+        }
+      },
+      complete: () => {
+        callback && callback();
+      }
+    });
+  },
+  setNews: function (res) {
+    let newsDetail = {
+        id: res.id,
+        title: res.title,
+        source: res.source,
+        // 先通过截取字符串位置方法取时间
+        // TODO:处理XSD时间格式
+        time: res.date.substring(11, 16),
+        imgUrl: res.firstImage,
+        readCount: res.readCount,
+        content : res.content
+    };
+    console.log(newsDetail);
+    this.setData({ newsDetail: newsDetail });
   }
 })

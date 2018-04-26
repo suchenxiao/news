124e0f303174dcaaf641727b9e8f6b49cb6cdc31
suchenxiao <suchenxiao@foxmail.com>
2018-04-26 20:41:59 +0800

    Feature: can change news-type in list-page

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,34 +1,28 @@
 //index.js
-
 Page({
   data: {
     newsTypes: [
-      { name: '国内', code: 'gn' },
-      { name: '国际', code: 'gj' },
-      { name: '财经', code: 'cj' },
-      { name: '娱乐', code: 'yl' },
-      { name: '军事', code: 'js' },
-      { name: '体育', code: 'ty' },
-      { name: '其他', code: 'other' }
+      { name: '国内', code: 'gn'},
+      { name: '国际', code: 'gj'},
+      { name: '财经', code: 'cj'},
+      { name: '娱乐', code: 'yl'},
+      { name: '军事', code: 'js'},
+      { name: '体育', code: 'ty'},
+      { name: '其他', code: 'other'}
     ],
+    newsType: '',
     newsList:[
       { 
-        id: '1519631218506',
-        title: '外媒称香港回归15年打破“经济将死”预言',
-        source: '中国新闻网',
-        time: '09:34',
-        imgUrl: 'http://img1.gtimg.com/news/pics/hv1/38/85/1076/69988613.jpg'
-      },
-      {
-        id: '1519631218591',
-        title: '实德指媒体窃公司机密 已向某媒体递律师函',
-        source: '腾讯财经',
-        time: '11:32',
-        imgUrl: 'http://img1.gtimg.com/finance/pics/hv1/33/207/1023/66573393.jpg'
+        id: '',
+        title: '',
+        source: '',
+        time: '',
+        imgUrl: ''
       }
     ]
   },
   onLoad: function() {
+    this.setNewsType(this.data.newsTypes[0].code);
     this.getNews();
   },
 
@@ -36,7 +30,7 @@ Page({
     wx.request({
       url: 'https://test-miniprogram.com/api/news/list',
       data: {
-        type: 'gn'
+        type: this.data.newsType
       },
       success: res => {
         let result = res.data.result;
@@ -61,13 +55,21 @@ Page({
       imgUrl : res[i].firstImage
       });
     }
-    console.log(newsList);
     this.setData({newsList : newsList});
   },
-  onTapNews: function(e) {
-    //console.log(e.currentTarget.dataset.id)
+  setNewsType(type, index){
+    this.setData({
+      newsType: type
+    });
+  },
+  onTapNewsItem: function(e) {
     wx.navigateTo({
       url: '/pages/detail/detail?id=' + e.currentTarget.dataset.id,
     })
   },
+  onTapNewsType: function (e) {
+    console.log(e);
+    this.setNewsType(e.currentTarget.dataset.type);
+    this.getNews();
+  }
 })
